acc44dee24c932122412cd70eb742dab5bb239e7
Omar-Codess <omartaki8@gmail.com>
2022-10-26 17:45:52 +0200

    creato array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,29 +1,61 @@
-const slides = [
-    {
-        image: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        image: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum.',
-    },
-    {
-        image: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        image: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-    },
-    {
-        image: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-    }
-];
+// const slides = [
+//     {
+//         image: 'img/01.jpg',
+//         title: 'Svezia',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+//     },
+//     {
+//         image: 'img/02.jpg',
+//         title: 'Svizzera',
+//         text: 'Lorem ipsum.',
+//     },
+//     {
+//         image: 'img/03.jpg',
+//         title: 'Gran Bretagna',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     },
+//     {
+//         image: 'img/04.jpg',
+//         title: 'Germania',
+//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+//     },
+//     {
+//         image: 'img/05.jpg',
+//         title: 'Paradise',
+//         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+//     }
+// ];
 
-console.log(slides);
+// console.log(slides);
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return{
+            slides: {
+                images: [
+                    'img/01.jpg',
+                    'img/02.jpg',
+                    'img/03.jpg',
+                    'img/04.jpg',
+                    'img/05.jpg',
+                ],
+                titles: [
+                    'Svezia',
+                    'Svizzera',
+                    'Gran Bretagna',
+                    'Germania',
+                    'Paradise',
+                ],
+                texts: [
+                    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                    'Lorem ipsum.',
+                    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+                ]
+            }
+        }
+    }
+}).mount(`#app`)
